d8eb0fbeedb56a82ed196602292686fe32924643
Avi Haiat <thaiat@ipelia.fr>
2016-06-23 15:05:56 +0300

    feat(app): Increase coverage

---
+++ test/mocha/index.spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var indexHelper = require('../../index');
+var expect = require('chai').expect;
+
+describe('index', () => {
+
+    it('should expose getUsername()', () => {
+        expect(indexHelper.getUsername).to.be.a('function');
+    });
+    it('should expose buildClient()', () => {
+        expect(indexHelper.buildClient).to.be.a('function');
+    });
+    it('should expose getClient()', () => {
+        expect(indexHelper.getClient).to.be.a('function');
+    });
+    it('should expose getRepo()', () => {
+        expect(indexHelper.getRepo).to.be.a('function');
+    });
+    it('should expose getAllRepos()', () => {
+        expect(indexHelper.getAllRepos).to.be.a('function');
+    });
+    it('should expose getFileAsBuffer()', () => {
+        expect(indexHelper.getFileAsBuffer).to.be.a('function');
+    });
+    it('should expose getPackageJson()', () => {
+        expect(indexHelper.getPackageJson).to.be.a('function');
+    });
+
+    it('should expose createTokenFile()', () => {
+        expect(indexHelper.createTokenFile).to.be.a('function');
+    });
+    it('should expose createRelease()', () => {
+        expect(indexHelper.createRelease).to.be.a('function');
+    });
+});
