d5300a8b8e41d3dc3d7591d73d5a79735a0859dd
Avi Haiat <thaiat@ipelia.fr>
2016-12-11 12:35:43 +0200

    fix(app): Expose all public from githubHelper

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 'use strict';
 var githubHelper = require('./lib/githubHelper');
 
-module.exports = {
-    getUsername: githubHelper.getUsername,
-    buildClient: githubHelper.buildClient,
-    getClient: githubHelper.getClient,
-    getRepo: githubHelper.getRepo,
-    getAllRepos: githubHelper.getAllRepos,
-    getFileAsBuffer: githubHelper.getFileAsBuffer,
-    getPackageJson: githubHelper.getPackageJson,
-    createTokenFile: githubHelper.createTokenFile,
-    createRelease: githubHelper.createRelease
-};
+module.exports = githubHelper;
--- test/mocha/index.spec.js
+++ test/mocha/index.spec.js
@@ -8,21 +8,27 @@ describe('index', () => {
     it('should expose getUsername()', () => {
         expect(indexHelper.getUsername).to.be.a('function');
     });
+
     it('should expose buildClient()', () => {
         expect(indexHelper.buildClient).to.be.a('function');
     });
+
     it('should expose getClient()', () => {
         expect(indexHelper.getClient).to.be.a('function');
     });
+
     it('should expose getRepo()', () => {
         expect(indexHelper.getRepo).to.be.a('function');
     });
+
     it('should expose getAllRepos()', () => {
         expect(indexHelper.getAllRepos).to.be.a('function');
     });
+
     it('should expose getFileAsBuffer()', () => {
         expect(indexHelper.getFileAsBuffer).to.be.a('function');
     });
+
     it('should expose getPackageJson()', () => {
         expect(indexHelper.getPackageJson).to.be.a('function');
     });
@@ -30,7 +36,16 @@ describe('index', () => {
     it('should expose createTokenFile()', () => {
         expect(indexHelper.createTokenFile).to.be.a('function');
     });
+
     it('should expose createRelease()', () => {
         expect(indexHelper.createRelease).to.be.a('function');
+    });
+
+    it('should expose getBranches()', () => {
+        expect(indexHelper.getBranches).to.be.a('function');
+    });
+
+    it('should expose getTree()', () => {
+        expect(indexHelper.getTree).to.be.a('function');
     });
 });
